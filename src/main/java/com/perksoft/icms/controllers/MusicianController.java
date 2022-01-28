be58f7f99fc2ec3f7a7cfb705fe271d1bdc40d84
eswarpalani@gmail.com <eswarpalani@gmail.com>
2022-01-28 11:49:58 +0530

    cross origin is added

--- src/main/java/com/perksoft/icms/controllers/MusicianController.java
+++ src/main/java/com/perksoft/icms/controllers/MusicianController.java
@@ -2,6 +2,7 @@ package com.perksoft.icms.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,10 @@ import com.perksoft.icms.models.Invoice;
 import com.perksoft.icms.models.Musician;
 import com.perksoft.icms.service.MusicianService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class MusicianController {
 	
